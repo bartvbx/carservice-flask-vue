9ef29731a905167696a840fff64788d4ef125372
Bart <bartoszty@gmail.com>
2022-10-04 22:47:27 +0200

    Add Marshmallow
    - Add Marshmallow
    - Temporary comment out services, clients, and visits blueprints

--- server/carservice/__init__.py
+++ server/carservice/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
 from .config import Config
 
 db = SQLAlchemy()
+ma = Marshmallow()
 
 
 def create_app(config_class=Config):
@@ -12,15 +14,22 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
     CORS(app, resources={r"/*": {'origins': "*"}})
     db.init_app(app)
+    ma.init_app(app)
+    
+    with app.app_context():
+        from carservice.views.parts import parts_bp
+        # from carservice.views.services import services_bp
+        # from carservice.views.clients import clients_bp
+        # from carservice.views.visits import visits_bp
 
-    from carservice.views.parts import parts_bp
-    from carservice.views.services import services_bp
-    from carservice.views.clients import clients_bp
-    from carservice.views.visits import visits_bp
+        app.register_blueprint(parts_bp)
+        # app.register_blueprint(services_bp)
+        # app.register_blueprint(clients_bp)
+        # app.register_blueprint(visits_bp)
 
-    app.register_blueprint(parts_bp)
-    app.register_blueprint(services_bp)
-    app.register_blueprint(clients_bp)
-    app.register_blueprint(visits_bp)
+        from .models import Part
 
-    return app
+        db.create_all()
+        db.session.commit()
+
+        return app
